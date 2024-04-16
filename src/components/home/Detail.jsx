1467d92f5a9b3c318a088b8259f6f0efc47fb866
megatech2000 <151417898+megatech2000@users.noreply.github.com>
2024-04-16 22:36:34 +0530

    updating
    changes

--- src/components/home/Detail.jsx
+++ src/components/home/Detail.jsx
@@ -15,14 +15,13 @@ const Detail = ({ Image }) => {
         </div>
         <div className="absolute top-0 bg-gradient-to-r from-[#000] to-transparent rounded-l-xl h-full w-[70%] "></div>
         <div className="absolute top-0 left-[20px] md:left-[100px] lg:left-[150px] flex flex-col items-start justify-center h-full">
-          <h1 className="mb-0 font-bold text-base md:text-6xl 2xl:text-[70px]">
+          <h1 className="mb-0 font-bold text-md md:text-6xl 2xl:text-[70px]">
             Elegance in <br />
             every detail
           </h1>
-          <p className="max-w-[300px] md:max-w-[500px] text-white text-sm py-3 md:py-10">
+          <p className="max-w-[300px] md:max-w-[500px] text-white text-base py-3 md:py-10">
             Lorem ipsum dolor sit amet consectetur platea nec ut augue
-            sollicitudin a sagittis vel suscipit amet lacus adipiscing eget
-            vitae neque in et in pulvinar enim.
+            sollicitudin a sagittis vel suscipit amet lacus adipiscing.
           </p>
           <div className=" flex gap-10 items-center justify-center ">
             <button className="px-4 py-1 md:px-6 md:py-2 bg-[#fff] text-black rounded-full btn-style">
