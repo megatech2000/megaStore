f82c9c2046b7af6416fd124ae60e0170e06e186d
megatech2000 <151417898+megatech2000@users.noreply.github.com>
2024-04-17 11:23:33 +0530

    updated
    product changes updated

--- src/app/products/components/Products.jsx
+++ src/app/products/components/Products.jsx
@@ -13,7 +13,112 @@ const Products = ({ Image }) => {
             />
           </div>
           <div>
-            <h5 className="text-black mb-0 py-5 font-bold">
+            <h5 className="text-black  mb-0 py-5 font-bold">
+              Wireless keyboard
+            </h5>
+            <p className="text-black text-sm md:text-base">
+              Lorem ipsum dolor sit amet consectetur tellus tellus sit posuere
+              arcu massa et
+            </p>
+            <p className="text-black pt-5 font-bold">$ 49.99 USD</p>
+          </div>
+        </div>
+        <div className="pb-10 cursor-pointer">
+          <div className="bg-gray-100 py-10 flex items-center justify-center rounded-3xl">
+            <Image
+              className="w-72 image-zoom"
+              src="/images/products/product-1.png"
+              alt="proucts images"
+              width={1000}
+              height={1000}
+            />
+          </div>
+          <div>
+            <h5 className="text-black  mb-0 py-5 font-bold">
+              Wireless keyboard
+            </h5>
+            <p className="text-black text-sm md:text-base">
+              Lorem ipsum dolor sit amet consectetur tellus tellus sit posuere
+              arcu massa et
+            </p>
+            <p className="text-black pt-5 font-bold">$ 49.99 USD</p>
+          </div>
+        </div>
+        <div className="pb-10 cursor-pointer">
+          <div className="bg-gray-100 py-10 flex items-center justify-center rounded-3xl">
+            <Image
+              className="w-72 image-zoom"
+              src="/images/products/product-1.png"
+              alt="proucts images"
+              width={1000}
+              height={1000}
+            />
+          </div>
+          <div>
+            <h5 className="text-black  mb-0 py-5 font-bold">
+              Wireless keyboard
+            </h5>
+            <p className="text-black text-sm md:text-base">
+              Lorem ipsum dolor sit amet consectetur tellus tellus sit posuere
+              arcu massa et
+            </p>
+            <p className="text-black pt-5 font-bold">$ 49.99 USD</p>
+          </div>
+        </div>
+        <div className="pb-10 cursor-pointer">
+          <div className="bg-gray-100 py-10 flex items-center justify-center rounded-3xl">
+            <Image
+              className="w-72 image-zoom"
+              src="/images/products/product-1.png"
+              alt="proucts images"
+              width={1000}
+              height={1000}
+            />
+          </div>
+          <div>
+            <h5 className="text-black  mb-0 py-5 font-bold">
+              Wireless keyboard
+            </h5>
+            <p className="text-black text-sm md:text-base">
+              Lorem ipsum dolor sit amet consectetur tellus tellus sit posuere
+              arcu massa et
+            </p>
+            <p className="text-black pt-5 font-bold">$ 49.99 USD</p>
+          </div>
+        </div>
+        <div className="pb-10 cursor-pointer">
+          <div className="bg-gray-100 py-10 flex items-center justify-center rounded-3xl">
+            <Image
+              className="w-72 image-zoom"
+              src="/images/products/product-1.png"
+              alt="proucts images"
+              width={1000}
+              height={1000}
+            />
+          </div>
+          <div>
+            <h5 className="text-black  mb-0 py-5 font-bold">
+              Wireless keyboard
+            </h5>
+            <p className="text-black text-sm md:text-base">
+              Lorem ipsum dolor sit amet consectetur tellus tellus sit posuere
+              arcu massa et
+            </p>
+            <p className="text-black pt-5 font-bold">$ 49.99 USD</p>
+          </div>
+        </div>
+        <div className="pb-10 cursor-pointer">
+          <div className="bg-gray-100 py-10 flex items-center justify-center rounded-3xl">
+            <Image
+              className="w-72 image-zoom"
+              src="/images/products/product-1.png"
+              alt="proucts images"
+              width={1000}
+              height={1000}
+            />
+          </div>
+          <div>
+            <h5 className="text-black  mb-0 py-5 font-bold">
               Wireless keyboard
             </h5>
             <p className="text-black text-sm md:text-base">
